6a2c2027aa10fc15108da4de2670a684193af158
Patrick M <patrick.v.murray@gmail.com>
2025-05-10 12:54:28 -0500

    one last thing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,30 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:5000",
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false,
-      },
+  base: "/", // ← ensure your index.html points to /assets/…
+  build: {
+    outDir: "dist",
+  },
+  proxy: {
+    "/api": {
+      target: "http://localhost:5000",
+      changeOrigin: true,
+      secure: false,
     },
   },
 });
